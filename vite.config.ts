a4dfb251948ac70363f559adc72458ff94e8d9b5
elmira1385 <elmirashokriyan64@gmail.com>
2026-02-08 01:15:46 +0330

    done

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "about.html",
+        services: "services.html",
+        portfolio: "portfolio.html",
+        price: "price.html",
+        contact: "contact.html"
+      }
+    }
+  }
 });
